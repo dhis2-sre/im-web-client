dcef8413ae4bfcb46b90449a2813ad7172ba8a7c
TonsV2 <sebastianthegreatful@gmail.com>
2022-10-26 10:54:21 +0700

    No more proxy List stacks Show stack details

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,16 @@ import { CssReset } from '@dhis2/ui'
 import InstancesList from "./components/Lists";
 import LoginPage from "./components/Login";
 import { IM_HOST, refreshApi } from "./api";
+import StackList from "./components/Stacks";
+import StackDetails from "./components/Stack";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
       <Route path="/" element={<LoginPage />} />
       <Route path="/instances" element={<InstancesList />} />
+      <Route path="/stacks" element={<StackList />} />
+      <Route path="/stacks/:name" element={<StackDetails />} />
     </Route>
   )
 );
