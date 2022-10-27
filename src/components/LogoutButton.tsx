7c4a1196ef4b1dbe20285491d9c6756ec1b4bcc7
mozafar <kabaros@gmail.com>
2022-10-27 09:45:29 +0200

    fix logout button

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,15 +1,19 @@
 import axios from 'axios'
 import { useAuthHeader } from 'react-auth-kit'
+import { useNavigate } from 'react-router'
 import { API_HOST } from '../api'
 import styles from './LogoutButton.module.css'
 
 const logout = (authHeader) =>
     axios
-        .delete('/signOut', {
+        .delete('/users', {
             baseURL: API_HOST,
             headers: {
                 Authorization: authHeader,
             },
+        })
+        .then(() => {
+            localStorage.removeItem('_auth')
         })
         .catch((error) => {
             console.error(error)
@@ -17,10 +21,12 @@ const logout = (authHeader) =>
 
 export const LogoutButton = () => {
     const getAuthHeader = useAuthHeader()
-
+    const navigate = useNavigate()
     return (
         <button
-            onClick={() => logout(getAuthHeader())}
+            onClick={() =>
+                logout(getAuthHeader()).then(() => navigate('/login'))
+            }
             className={styles.logout}
         >
             logout
