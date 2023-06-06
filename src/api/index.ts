056ebce5456bf383f1af6987d0c5621cf217d2ee
HendrikThePendric <hendrik@dhis2.org>
2023-06-06 12:43:18 +0200

    fix: read refresh expiry from local storage and fix refresh logic todos

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { createRefresh } from 'react-auth-kit'
 import { InstancesGroup } from '../types'
+import { parseToken } from '../modules'
 
 export const API_HOST =
     process.env.REACT_APP_IM_API || 'https://api.im.dev.test.c.dhis2.org'
@@ -36,37 +37,51 @@ export const getToken = (username, password) => {
     )
 }
 
+const getRefreshIntervalFromLocalStorage = () => {
+    const refreshToken = localStorage.getItem('_auth_refresh')
+
+    if (!refreshToken) {
+        throw new Error(
+            'Tried to read refresh token expiry from local storage but found no token'
+        )
+    }
+
+    const { expiryDurationInMinutes } = parseToken(refreshToken)
+
+    return expiryDurationInMinutes
+}
+
 export const refreshApi = createRefresh({
-    // TODO: this expiration value should read from the token... egg/chicken?
-    interval: 14, // Refresh the token every 14 minutes
-    refreshApiCallback: ({
-        authToken,
-        authTokenExpireAt,
-        refreshToken,
-        refreshTokenExpiresAt,
-        authUserState,
-    }) => {
+    interval: getRefreshIntervalFromLocalStorage(),
+    refreshApiCallback: ({ refreshToken }) => {
         return axios
-            .post(`${API_HOST}/refresh`, {
-                refreshToken: refreshToken,
-                // TODO: no old auth token... https://api.im.dev.test.c.dhis2.org/users/docs#operation/refreshToken
-                //                oldAuthToken: authToken,
-            })
+            .post(`${API_HOST}/refresh`, { refreshToken })
             .then(({ data }) => {
+                const {
+                    expiryDurationInMinutes: newAuthTokenExpireIn,
+                    user: newAuthUserState,
+                } = parseToken(data.access_token)
+                const { expiryDurationInMinutes: newRefreshTokenExpiresIn } =
+                    parseToken(data.refresh_token)
+
                 return {
                     isSuccess: true, // For successful network request isSuccess is true
                     newAuthToken: data.access_token,
+                    newAuthTokenExpireIn,
                     newRefreshToken: data.refresh_token,
-                    newAuthTokenExpireIn: data.expires_in,
-                    // You can also add new refresh token ad new user state
+                    newRefreshTokenExpiresIn,
+                    newAuthUserState,
                 }
             })
-            .catch((e) => {
-                console.error(e)
+            .catch((error) => {
+                console.error('Could not refresh access token', error)
                 return {
-                    isSuccess: false, // For unsuccessful network request isSuccess is false
+                    isSuccess: false, // For successful network request isSuccess is true
                     newAuthToken: null,
                     newAuthTokenExpireIn: null,
+                    newRefreshToken: null,
+                    newRefreshTokenExpiresIn: null,
+                    newAuthUserState: null,
                 }
             })
     },
