aceea72233f583659bd80b39ab2d2189003962c5
HendrikThePendric <hendrik@dhis2.org>
2023-06-06 12:40:23 +0200

    refactor: add token parser that adds expiry in minutes to parsed token

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,1 +1,2 @@
 export { clearAuthItemsFromLocalStorage } from './clearAuthItemsFromLocalStorage'
+export { parseToken } from './parseToken'
---
+++ src/modules/parseToken.ts
@@ -0,0 +1,21 @@
+import jwtDecode from 'jwt-decode'
+
+type Token = {
+    exp: number
+    iat: number
+    user?: any
+}
+interface ParsedToken extends Token {
+    expiryDurationInMinutes: number
+}
+
+export const parseToken = (token: string): ParsedToken => {
+    const decodedToken: Token = jwtDecode(token)
+
+    return {
+        ...decodedToken,
+        expiryDurationInMinutes: Math.floor(
+            (decodedToken.exp - decodedToken.iat) / 60
+        ),
+    }
+}
