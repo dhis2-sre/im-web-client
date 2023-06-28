e79411c08a5152fd246e7f6510ed806a9cb1d927
HendrikThePendric <hendrik@dhis2.org>
2023-06-28 10:50:41 +0200

    chore: use double quotes for `.yml`

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,4 +3,12 @@ const { config } = require('@dhis2/cli-style')
 module.exports = {
     ...require(config.prettier),
     printWidth: 180,
+    overrides: [
+        {
+            files: '**/*.yml',
+            options: {
+                singleQuote: false,
+            },
+        },
+    ],
 }
