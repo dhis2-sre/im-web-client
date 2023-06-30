37529575ddca9be8ea3ce171b9109765debdfacb
TonsV2 <sebastianthegreatful@gmail.com>
2023-06-30 13:57:20 +0800

    chore: remove log

--- src/components/InstancesLists.tsx
+++ src/components/InstancesLists.tsx
@@ -146,7 +146,6 @@ const InstancesList = () => {
                                 })}
                             </TableBody>
                         </DataTable>
-                        <pre>{log}</pre>
                     </div>
                 )
             })}
