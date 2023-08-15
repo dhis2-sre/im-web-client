cd32619a48e0d72818daf175f7a09ec75a0b54ed
HendrikThePendric <hendrik@dhis2.org>
2023-08-15 17:27:03 +0200

    chore: run in background pls

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,7 +70,8 @@ export default defineConfig({
 
     /* Run your local dev server before starting the tests */
     webServer: {
-        command: 'yarn react-scripts start',
+        // Run in the background
+        command: '(yarn start&)',
         url: 'http://localhost:3000',
         reuseExistingServer: !process.env.CI,
     },
