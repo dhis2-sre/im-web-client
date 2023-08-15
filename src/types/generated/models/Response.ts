4665d32b6c1bd885720163a9daf6c4e5d8bd45f9
HendrikThePendric <hendrik@dhis2.org>
2023-08-15 09:26:08 +0200

    chore: delete generated types to see changes on the next commit

--- src/types/generated/models/Response.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-/**
- * Response depends on the input and can be either a list or a map
- */
-export type Response = {}
