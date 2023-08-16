184ffa1fbcce8b07f7f2bb59b52fe1e7ad60f76c
HendrikThePendric <hendrik@dhis2.org>
2023-08-16 10:54:52 +0200

    chore: undo setting change

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,8 +73,7 @@ export default defineConfig({
     webServer: {
         command: 'BROWSER=none yarn start',
         url: 'http://127.0.0.1:3000',
-        // reuseExistingServer: !process.env.CI,
-        reuseExistingServer: false,
+        reuseExistingServer: !process.env.CI,
         stderr: 'pipe',
         stdout: 'pipe',
     },
