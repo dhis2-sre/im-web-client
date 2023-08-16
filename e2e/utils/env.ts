06e7683361265100ad02aba66a841724e28dc36f
HendrikThePendric <hendrik@dhis2.org>
2023-08-16 13:11:11 +0200

    chore: tweak env variable names to match GH secrets

--- e2e/utils/env.ts
+++ e2e/utils/env.ts
@@ -5,6 +5,5 @@ import path from 'path'
 const env = dotenv.config({ path: path.resolve(__dirname, '../../.env.local') })
 dotenvExpand.expand(env)
 
-export const target = process.env.UI_HOSTNAME
-export const username = process.env.USER_EMAIL
-export const password = process.env.PASSWORD
+export const username = process.env.IM_ADMIN_USER
+export const password = process.env.IM_ADMIN_PASSWORD
