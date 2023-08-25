bdf72eb6e8cc7b51b0ff87e4cdb57e35a1c6d401
tonsV2 <tonsV2@users.noreply.github.com>
2023-08-25 10:26:57 +0800

    feat: add remove users from group (#202)
    * feat: add user to group
    
    * feat: remove user from group

---
+++ src/views/users/remove-from-group-button.tsx
@@ -0,0 +1,51 @@
+import { Button, Center, CircularLoader, IconSubtract16 } from '@dhis2/ui'
+import type { FC } from 'react'
+import { useCallback } from 'react'
+import { useAuthAxios } from '../../hooks'
+import { useAlert } from '@dhis2/app-service-alerts'
+
+type RemoveFromGroupButtonProps = {
+    group: String
+    userId: Number
+    onComplete: Function
+}
+
+export const RemoveFromGroupButton: FC<RemoveFromGroupButtonProps> = ({ group, userId, onComplete }) => {
+    const [{ loading }, removeUser] = useAuthAxios(
+        {
+            url: `/groups/${group}/users/${userId}`,
+            method: 'delete',
+        },
+        { manual: true }
+    )
+
+    const { show: showAlert } = useAlert(
+        ({ message }) => message,
+        ({ isCritical }) => (isCritical ? { critical: true } : { success: true })
+    )
+
+    const submit = useCallback(async () => {
+        try {
+            await removeUser()
+            showAlert({ message: 'User removed from group successfully', isCritical: false })
+        } catch (error) {
+            showAlert({ message: 'Error while removing user from group', isCritical: true })
+            console.log(error)
+        }
+        onComplete()
+    }, [onComplete, removeUser, showAlert])
+
+    if (loading) {
+        return (
+            <Center>
+                <CircularLoader />
+            </Center>
+        )
+    }
+
+    return (
+        <>
+            <Button icon={<IconSubtract16 />} onClick={submit} />
+        </>
+    )
+}
