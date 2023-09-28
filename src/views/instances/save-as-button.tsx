990507ab76c203d8da558ebd3de9e7d1845a1aa3
TonsV2 <sebastianthegreatful@gmail.com>
2023-09-28 16:35:28 +0800

    chore: formatting

--- src/views/instances/save-as-button.tsx
+++ src/views/instances/save-as-button.tsx
@@ -1,14 +1,14 @@
-import {Button, IconSave16} from '@dhis2/ui'
-import type {FC} from 'react'
-import {useState} from 'react'
-import {SaveAsModal} from "./save-as-modal";
+import { Button, IconSave16 } from '@dhis2/ui'
+import type { FC } from 'react'
+import { useState } from 'react'
+import { SaveAsModal } from './save-as-modal'
 
 type AddToGroupButtonProps = {
     instanceId: Number
     instanceName: string
 }
 
-export const SaveAsButton: FC<AddToGroupButtonProps> = ({instanceId, instanceName}) => {
+export const SaveAsButton: FC<AddToGroupButtonProps> = ({ instanceId, instanceName }) => {
     const [showModal, setShowModal] = useState<boolean>(false)
 
     return (
@@ -16,8 +16,7 @@ export const SaveAsButton: FC<AddToGroupButtonProps> = ({instanceId, instanceNam
             <Button small secondary icon={<IconSave16 />} onClick={() => setShowModal(true)}>
                 Save as
             </Button>
-            {showModal &&
-                <SaveAsModal onClose={() => setShowModal(false)} instanceId={instanceId} instanceName={instanceName}/>}
+            {showModal && <SaveAsModal onClose={() => setShowModal(false)} instanceId={instanceId} instanceName={instanceName} />}
         </>
     )
 }
