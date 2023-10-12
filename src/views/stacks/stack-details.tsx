f3aa4eca4df7ab6f7a75003e77528c611ede5aa4
TonsV2 <sebastianthegreatful@gmail.com>
2023-10-12 15:01:41 +0700

    chore: simplify sorting
    Signed-off-by: TonsV2 <sebastianthegreatful@gmail.com>

--- src/views/stacks/stack-details.tsx
+++ src/views/stacks/stack-details.tsx
@@ -3,21 +3,12 @@ import { useParams } from 'react-router-dom'
 import { useAuthAxios } from '../../hooks'
 import { Stack } from '../../types'
 import styles from './stack-details.module.css'
-import { useEffect, useState } from 'react'
 
 export const StackDetails = () => {
     const { name } = useParams()
     const [{ data: stack, loading, error }] = useAuthAxios<Stack>(`stacks/${name}`)
-    const [sortedStack, setSortedStack] = useState<Stack>()
 
-    useEffect(() => {
-        if (stack) {
-            stack.parameters.sort((a, b) => (a.priority < b.priority ? -1 : 1))
-            setSortedStack(stack)
-        }
-    }, [stack])
-
-    if (loading || !sortedStack) {
+    if (loading) {
         return (
             <Center>
                 <CircularLoader />
@@ -47,15 +38,17 @@ export const StackDetails = () => {
                 </DataTableHead>
 
                 <DataTableBody>
-                    {sortedStack.parameters?.map((parameter) => {
-                        return (
-                            <DataTableRow key={parameter.name}>
-                                <DataTableCell>{parameter.name}</DataTableCell>
-                                <DataTableCell>{parameter.defaultValue}</DataTableCell>
-                                <DataTableCell>{parameter.consumed.toString()}</DataTableCell>
-                            </DataTableRow>
-                        )
-                    })}
+                    {stack.parameters
+                        ?.sort((a, b) => (a.priority < b.priority ? -1 : 1))
+                        .map((parameter) => {
+                            return (
+                                <DataTableRow key={parameter.name}>
+                                    <DataTableCell>{parameter.name}</DataTableCell>
+                                    <DataTableCell>{parameter.defaultValue}</DataTableCell>
+                                    <DataTableCell>{parameter.consumed.toString()}</DataTableCell>
+                                </DataTableRow>
+                            )
+                        })}
                 </DataTableBody>
             </DataTable>
         </div>
