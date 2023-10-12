adc5d424d7519b5d435ba77da87b2c7c40e6930d
tonsV2 <tonsV2@users.noreply.github.com>
2023-10-12 18:01:26 +0800

    Save as (#238)
    * feat: save as
    
    * chore: format
    
    * chore: formatting
    
    * chore: update label
    
    * chore: show file extension and append automatically to file name
    
    * chore: formatting
    
    Signed-off-by: TonsV2 <sebastianthegreatful@gmail.com>
    
    * Update src/views/instances/save-as-modal.tsx
    
    Co-authored-by: Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
    
    * chore: use an object rather than an array
    
    Signed-off-by: TonsV2 <sebastianthegreatful@gmail.com>
    
    * chore: remove "any"
    
    Signed-off-by: TonsV2 <sebastianthegreatful@gmail.com>
    
    * chore: align input field and file extension
    
    Signed-off-by: TonsV2 <sebastianthegreatful@gmail.com>
    
    ---------
    
    Signed-off-by: TonsV2 <sebastianthegreatful@gmail.com>
    Co-authored-by: Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>

--- src/views/instances/instances-list.tsx
+++ src/views/instances/instances-list.tsx
@@ -24,6 +24,7 @@ import { OpenButton } from './open-button'
 import { ResetButton } from './reset-button'
 import { RestartButton } from './restart-button'
 import { LogButton } from './log-button'
+import { SaveAsButton } from './save-as-button'
 import type { FC } from 'react'
 
 const calculateExpiration = (instance: Instance) => new Date(instance.createdAt).getTime() + instance.ttl * 1000
@@ -101,6 +102,7 @@ export const InstancesList: FC = () => {
                                                     <RestartButton instanceId={instance.id} instanceName={instance.name} onComplete={refetch} />
                                                     <ResetButton instanceId={instance.id} instanceName={instance.name} onComplete={refetch} />
                                                     <DeleteButton instanceId={instance.id} instanceName={instance.name} onComplete={refetch} />
+                                                    <SaveAsButton instanceId={instance.id} instanceName={instance.name} />
                                                 </ButtonStrip>
                                             </DataTableCell>
                                         </DataTableRow>
