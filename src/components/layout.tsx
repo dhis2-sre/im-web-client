0f5bec1a2f4b9ad73a6c421d89abcfd3d14dc65b
HendrikThePendric <hendrik@dhis2.org>
2023-11-09 15:39:50 +0100

    feat: add button in sidenav to create new dhis2 instance

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,23 @@
-import { LogoIconWhite } from '@dhis2/ui'
-import { Navigate, NavLink, Outlet, useLocation } from 'react-router-dom'
+import { LogoIconWhite, IconAdd16, Tooltip } from '@dhis2/ui'
+import { Navigate, NavLink, Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { LogoutButton } from './logout-button'
 import styles from './layout.module.css'
 import type { FC } from 'react'
+import { useCallback } from 'react'
 import { useAuth } from '../hooks'
 
 export const Layout: FC = () => {
+    const navigate = useNavigate()
     const { isAdministrator } = useAuth()
     const location = useLocation()
+    const toNewDhis2Instance = useCallback(
+        (event) => {
+            event.stopPropagation()
+            event.preventDefault()
+            navigate('/instances/new-dhis2')
+        },
+        [navigate]
+    )
 
     if (location.pathname === '/') {
         return <Navigate to="/instances" />
@@ -21,7 +31,14 @@ export const Layout: FC = () => {
                     Instance Manager
                 </h1>
                 <nav className={styles.navlist}>
-                    <NavLink to="/instances">Instances</NavLink>
+                    <NavLink to="/instances">
+                        Instances
+                        <Tooltip content="Create new DHIS2 Core instance">
+                            <button onClick={toNewDhis2Instance} className={styles.toNewDhis2Instance}>
+                                <IconAdd16 />
+                            </button>
+                        </Tooltip>
+                    </NavLink>
                     <NavLink to="/databases">Databases</NavLink>
                     <NavLink to="/stacks">Stacks</NavLink>
                     {isAdministrator && (
