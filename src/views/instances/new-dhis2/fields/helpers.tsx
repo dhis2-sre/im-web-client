6df4242dcef068b20158fedc5498a271cb7d8fb9
radnov <radnov@pm.me>
2023-11-24 18:49:34 +0200

    refactor: remove toTitleCase helper function

--- src/views/instances/new-dhis2/fields/helpers.tsx
+++ src/views/instances/new-dhis2/fields/helpers.tsx
@@ -1,9 +1,3 @@
-export const toTitleCase = (str: string): string =>
-    str
-        .toLowerCase()
-        .replace(/^[-_]*(.)/, (_, c) => c.toUpperCase())
-        .replace(/[-_]+(.)/g, (_, c) => ' ' + c.toUpperCase())
-
 export const mapStringToValueLabel = (str: string): { value: string; label: string } => ({ value: str, label: str })
 
 export const parseBool = (bool: any): string => (typeof bool === 'boolean' && bool ? 'true' : 'false')
