75bb1edff9b670ad131d6edb4d84fde9665ae936
Rado <radoslav@dhis2.org>
2023-12-13 18:34:05 +0200

    chore: add new generated type GroupsWithDeployments
    Signed-off-by: Rado <radoslav@dhis2.org>

---
+++ src/types/generated/models/GroupsWithDeployments.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { Deployment } from './Deployment'
+
+export type GroupsWithDeployments = {
+    deployments?: Array<Deployment>
+    hostname?: string
+    name?: string
+}
