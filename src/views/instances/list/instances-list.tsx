adce3091c06294ee9ef230fc69c3217c8c7307ab
HendrikThePendric <hendrik@dhis2.org>
2023-12-19 17:24:12 +0100

    feat: render all groups in a single table so the columns are aligned

--- src/views/instances/list/instances-list.tsx
+++ src/views/instances/list/instances-list.tsx
@@ -1,33 +1,41 @@
 import {
     Button,
     ButtonStrip,
+    Center,
+    CircularLoader,
     colors,
     DataTable,
-    DataTableBody,
     DataTableCell,
     DataTableColumnHeader,
-    DataTableHead,
     DataTableRow,
-    DataTableToolbar,
     IconAdd24,
     IconWorld24,
     NoticeBox,
+    Tag,
 } from '@dhis2/ui'
 import type { FC } from 'react'
 import Moment from 'react-moment'
 import { NavLink, useNavigate } from 'react-router-dom'
 import { useAuthAxios } from '../../../hooks'
-import { GroupsWithInstances, Instance } from '../../../types'
+import { Deployment, GroupsWithDeployments } from '../../../types'
 import { OpenButton } from './open-button'
 import { StatusLabel } from './status-label'
 import styles from './instances-list.module.css'
 import { ActionsDropdownMenu } from './actions-dropdown-menu'
 
-const calculateExpiration = (instance: Instance) => new Date(instance.createdAt).getTime() + instance.ttl * 1000
+type CustomizedDeployment = Deployment & {
+    public: boolean
+    ttl: number
+}
+interface CustomizedGroupsWithDeployments extends GroupsWithDeployments {
+    deployments?: Array<CustomizedDeployment>
+}
+
+const calculateExpiration = (deployment: CustomizedDeployment) => new Date(deployment.createdAt).getTime() + deployment.ttl * 1000
 
 export const InstancesList: FC = () => {
     const navigate = useNavigate()
-    const [{ data, error }, refetch] = useAuthAxios<GroupsWithInstances[]>('instances', {
+    const [{ data, error, loading }, refetch] = useAuthAxios<CustomizedGroupsWithDeployments[]>('/deployments', {
         useCache: false,
     })
 
@@ -47,65 +55,77 @@ export const InstancesList: FC = () => {
                 </NoticeBox>
             )}
 
-            {data?.length < 1 && <h3>No instances</h3>}
+            {loading && (
+                <Center className={styles.loaderWrap}>
+                    <CircularLoader />
+                </Center>
+            )}
 
-            {data?.map((group) => {
-                return (
-                    <div key={group.name}>
-                        <DataTableToolbar className={styles.tabletoolbar}>{group.name}</DataTableToolbar>
-                        <DataTable>
-                            <DataTableHead>
+            {data?.length === 0 && <h3>No instances</h3>}
+
+            {data?.length > 0 && (
+                <DataTable>
+                    {data?.map((group) => {
+                        return (
+                            <>
+                                <DataTableRow>
+                                    <DataTableCell colspan="9">
+                                        <h2 className={styles.groupName}>{group.name}</h2>
+                                    </DataTableCell>
+                                </DataTableRow>
                                 <DataTableRow>
                                     <DataTableColumnHeader>Status</DataTableColumnHeader>
                                     <DataTableColumnHeader>Name</DataTableColumnHeader>
                                     <DataTableColumnHeader>Description</DataTableColumnHeader>
+                                    <DataTableColumnHeader>Components</DataTableColumnHeader>
                                     <DataTableColumnHeader>Created</DataTableColumnHeader>
                                     <DataTableColumnHeader>Updated</DataTableColumnHeader>
                                     <DataTableColumnHeader>Owner</DataTableColumnHeader>
-                                    <DataTableColumnHeader>Type</DataTableColumnHeader>
                                     <DataTableColumnHeader>Expires</DataTableColumnHeader>
                                     <DataTableColumnHeader></DataTableColumnHeader>
                                 </DataTableRow>
-                            </DataTableHead>
 
-                            <DataTableBody>
-                                {group.instances?.map((instance) => {
+                                {group.deployments?.map((deployment) => {
                                     return (
-                                        <DataTableRow key={instance.id}>
+                                        <DataTableRow key={deployment.id}>
                                             <DataTableCell>
-                                                <StatusLabel instanceId={instance.id} />
+                                                <StatusLabel instanceId={deployment.id} />
                                             </DataTableCell>
                                             <DataTableCell>
                                                 <span className={styles.verticallyAlignedCellContent}>
-                                                    {instance.name} {instance.public && <IconWorld24 color={colors.grey600} />}
+                                                    {deployment.name} {deployment.public && <IconWorld24 color={colors.grey600} />}
                                                 </span>
                                             </DataTableCell>
-                                            <DataTableCell>{instance.description}</DataTableCell>
+                                            <DataTableCell>{deployment.description}</DataTableCell>
                                             <DataTableCell>
-                                                <Moment date={instance.createdAt} fromNow />
+                                                {deployment.instances?.map(({ stackName }) => (
+                                                    <Tag className={styles.stackNameTag}>{stackName}</Tag>
+                                                ))}
                                             </DataTableCell>
                                             <DataTableCell>
-                                                <Moment date={instance.updatedAt} fromNow />
+                                                <Moment date={deployment.createdAt} fromNow />
                                             </DataTableCell>
-                                            <DataTableCell>{instance.user.email}</DataTableCell>
-                                            <DataTableCell>{instance.stackName}</DataTableCell>
                                             <DataTableCell>
-                                                <Moment date={calculateExpiration(instance)} fromNow />
+                                                <Moment date={deployment.updatedAt} fromNow />
+                                            </DataTableCell>
+                                            <DataTableCell>{deployment.user.email}</DataTableCell>
+                                            <DataTableCell>
+                                                <Moment date={calculateExpiration(deployment)} fromNow />
                                             </DataTableCell>
                                             <DataTableCell>
                                                 <ButtonStrip>
-                                                    <OpenButton hostname={group.hostname} instanceName={instance.name} />
-                                                    <ActionsDropdownMenu id={instance.id} name={instance.name} refreshList={refetch} />
+                                                    <OpenButton hostname={group.hostname} instanceName={deployment.name} />
+                                                    <ActionsDropdownMenu id={deployment.id} name={deployment.name} refreshList={refetch} />
                                                 </ButtonStrip>
                                             </DataTableCell>
                                         </DataTableRow>
                                     )
                                 })}
-                            </DataTableBody>
-                        </DataTable>
-                    </div>
-                )
-            })}
+                            </>
+                        )
+                    })}
+                </DataTable>
+            )}
         </div>
     )
 }
