83082ad372d8fc0553aa707e62ad24f8e596f6e5
Andreas Jensen <andreas@dhis2.org>
2024-02-07 11:25:10 +0700

    test: fix label s/username/email/
    Signed-off-by: Andreas Jensen <andreas@dhis2.org>

--- e2e/utils/login.ts
+++ e2e/utils/login.ts
@@ -3,8 +3,8 @@ import { password, username } from './env'
 
 export const login = async (page) => {
     await page.goto('/login')
-    await page.getByLabel('username').click()
-    await page.getByLabel('username').fill(username)
+    await page.getByLabel('email').click()
+    await page.getByLabel('email').fill(username)
     await page.getByLabel('password').click()
     await page.getByLabel('password').fill(password)
     await page.getByRole('button', { name: 'Login' }).click()
