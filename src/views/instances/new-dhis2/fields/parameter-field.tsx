6f10491d856330cdfb97b5bcc9befa8d5627e98a
Andreas Jensen <andreas@dhis2.org>
2024-04-03 19:04:29 +0800

    feat: dhis2 persistent storage
    Signed-off-by: Andreas Jensen <andreas@dhis2.org>

--- src/views/instances/new-dhis2/fields/parameter-field.tsx
+++ src/views/instances/new-dhis2/fields/parameter-field.tsx
@@ -1,5 +1,14 @@
 import type { FC } from 'react'
-import { IMAGE_TAG, IMAGE_REPOSITORY, DATABASE_ID, IMAGE_PULL_POLICY, FLYWAY_MIGRATE_OUT_OF_ORDER, FLYWAY_REPAIR_BEFORE_MIGRATION, INSTALL_REDIS } from '../constants'
+import {
+    IMAGE_TAG,
+    IMAGE_REPOSITORY,
+    DATABASE_ID,
+    IMAGE_PULL_POLICY,
+    FLYWAY_MIGRATE_OUT_OF_ORDER,
+    FLYWAY_REPAIR_BEFORE_MIGRATION,
+    INSTALL_REDIS,
+    ENABLE_PERSISTENT_STORAGE
+} from '../constants'
 import { IntergrationParameterSelect } from './intergration-parameter-select'
 import { ImageRepositorySelect } from './image-repository-select'
 import { ImageTagSelect } from './image-tag-select'
@@ -22,6 +31,7 @@ export const ParameterField: FC<ParameterFieldProps> = ({ stackId, displayName, 
         case DATABASE_ID:
         case IMAGE_PULL_POLICY:
             return <IntergrationParameterSelect stackId={stackId} parameterName={parameterName} displayName={displayName} />
+        case ENABLE_PERSISTENT_STORAGE:
         case FLYWAY_MIGRATE_OUT_OF_ORDER:
         case FLYWAY_REPAIR_BEFORE_MIGRATION:
         case INSTALL_REDIS:
