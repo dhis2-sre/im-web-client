b1c71d55e7c3f8621f6366b95b8a9d65f48c058c
Chisom Chima <87203527+Chisomchima@users.noreply.github.com>
2024-07-09 20:02:43 +0100

    feat: client-side filtering of instance (#497)
    * feat: server-side filtering of instances
    
    * fix: cleanups and improvement on useDeployments file
    
    * fix: get current user from authcontext
    
    * fix: change file name to kebab case
    
    ---------
    
    Co-authored-by: tonsV2 <tonsV2@users.noreply.github.com>

---
+++ src/views/instances/list/filter-deployments.tsx
@@ -0,0 +1,33 @@
+import { useAuthAxios } from '../../../hooks'
+import { GroupsWithDeployments } from '../../../types'
+import { useState, useMemo, useContext } from 'react'
+import { AuthContext } from '../../../contexts/auth-context'
+
+const useDeployments = () => {
+    const [{ data, error, loading }, refetch] = useAuthAxios<GroupsWithDeployments[]>('/deployments', {
+        useCache: false,
+    })
+    const [showOnlyMyInstances, setShowOnlyMyInstances] = useState(false)
+    const { currentUser } = useContext(AuthContext)
+
+
+    const filterDeploymentsByUser = useMemo(() => {
+        return data?.map(group => ({
+            ...group,
+            deployments: group.deployments.filter(deployment => deployment.user.id === currentUser.id)
+        }))
+    }, [data, currentUser]);
+
+    const filteredData = showOnlyMyInstances ? filterDeploymentsByUser : data;
+
+    return {
+        data: filteredData,
+        error,
+        loading,
+        refetch,
+        showOnlyMyInstances,
+        setShowOnlyMyInstances,
+    }
+}
+
+export default useDeployments
