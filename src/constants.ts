ea3e6bf416eec8463ccd4c64824d8ae509e5e343
chisom chima <chimachisom360@gmail.com>
2024-07-16 08:05:53 +0100

    fix: add stack names to constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,9 @@
+import { Dhis2StackName } from './views/instances/new-dhis2/parameter-fieldset'
+
+export const STACK_NAMES: Record<string, Dhis2StackName> = {
+    DB: 'dhis2-db',
+    CORE: 'dhis2-core',
+    PG_ADMIN: 'pgadmin',
+}
+
 export const VIEWABLE_INSTANCE_TYPES = ['pgadmin', 'dhis2-core']
--- src/views/instances/details/view-instance-menu-item.tsx
+++ src/views/instances/details/view-instance-menu-item.tsx
@@ -1,5 +1,6 @@
 import { Button, IconLaunch16 } from '@dhis2/ui'
 import type { FC } from 'react'
+import { STACK_NAMES } from '../../../constants'
 
 type ViewInstanceMenuItemProps = {
     groupName: string
@@ -9,7 +10,7 @@ type ViewInstanceMenuItemProps = {
 
 export const ViewInstanceMenuItem: FC<ViewInstanceMenuItemProps> = ({ groupName, name, stackName }) => {
     const host = `${groupName}.im.dhis2.org`
-    const path = stackName === 'pgadmin' ? `${name}-pgadmin` : name
+    const path = stackName === STACK_NAMES.PG_ADMIN ? `${name}-pgadmin` : name
     const url = `https://${host}/${path}`
 
     return <Button small secondary icon={<IconLaunch16 />} onClick={() => window.open(url, '_blank', 'noopener,noreferrer')}>Open</Button>
