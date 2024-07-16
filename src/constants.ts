a52aec51bcbc98eff329cbc7493b183bf2d9ec57
chisom chima <chimachisom360@gmail.com>
2024-07-16 07:59:34 +0100

    fix: add constants

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+import { Dhis2StackName } from './views/instances/new-dhis2/parameter-fieldset'
+
+export const STACK_NAMES: Record<string, Dhis2StackName> = {
+    DB: 'dhis2-db',
+    CORE: 'dhis2-core',
+    PG_ADMIN: 'pgadmin',
+}
