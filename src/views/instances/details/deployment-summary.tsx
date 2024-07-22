16dfb2a6e8ca3cce3d0c02dc02e82c7d9fe185a9
chisom chima <chimachisom360@gmail.com>
2024-07-22 11:00:18 +0100

    fix: remove spacing and assertions

--- src/views/instances/details/deployment-summary.tsx
+++ src/views/instances/details/deployment-summary.tsx
@@ -7,35 +7,33 @@ import { IconClock16, IconUser16, IconUserGroup16, IconWarning16, IconClockHisto
 import styles from './deployment-summary.module.css'
 
 export const DeploymentSummary: FC<{ deployment: Deployment }> = ({ deployment }) => {
-
     return (
         <div className={styles.wrapper}>
-            <h2 className={styles.title}>{deployment.name || ' - '}</h2>
-            <hr className={styles.hr} />
+            <h2 className={styles.title}>{deployment.name}</h2>
             <div className={styles.row}>
                 <div className={styles.singleDetails}>
                     <IconClock16 />
-                    <p>Created {deployment.createdAt ? <Moment date={deployment.createdAt} fromNow /> : ' - '}</p>
+                    <p>Created <Moment date={deployment.createdAt} fromNow /></p>
                 </div>
                 <div className={styles.singleDetails}>
                     <IconWarning16 />
-                    <p>Stopping in {deployment.ttl ? <MomentExpiresFromNow createdAt={deployment.createdAt} ttl={deployment.ttl} /> : ' - '}</p>
+                    <p>Stopping in <MomentExpiresFromNow createdAt={deployment.createdAt} ttl={deployment.ttl} /></p>
                 </div>
                 <div className={styles.singleDetails}>
                     <IconUser16 />
-                    <p>Created by {deployment.user?.email || ' - '}</p>
+                    <p>Created by {deployment.user.email}</p>
                 </div>
                 <div className={styles.singleDetails}>
                     <IconUserGroup16 />
-                    <p>User group {deployment.groupName || ' - '}</p>
+                    <p>User group {deployment.groupName}</p>
                 </div>
                 <div className={styles.singleDetails}>
                     <IconWorld16 />
-                    <p>Public {deployment.public === true ? 'Yes' : 'No'}</p>
+                    <p>Public {deployment.public ? 'Yes' : 'No'}</p>
                 </div>
                 <div className={styles.singleDetails}>
                     <IconClockHistory16 />
-                    <p>Updated {deployment.updatedAt ? <Moment date={deployment.updatedAt} fromNow /> : ' - '}</p>
+                    <p>Updated <Moment date={deployment.updatedAt} fromNow /></p>
                 </div>
                 <div className={styles.singleDetails}>
                     <IconInfo16 />
