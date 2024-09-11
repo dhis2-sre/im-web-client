3428b9005961f5494e44e1db9620998f101780c2
Chisomchima <chimachisom360@gmail.com>
2024-09-11 12:30:45 +0100

    feat: implement filtering in single select component

--- src/components/searchable-single-select.tsx
+++ src/components/searchable-single-select.tsx
@@ -1,6 +1,6 @@
 import i18n from '@dhis2/d2-i18n'
-import { CircularLoader, Input, SingleSelect, SingleSelectOption } from '@dhis2/ui'
-import React, { forwardRef, useEffect, useState } from 'react'
+import { Input, SingleSelect, SingleSelectOption } from '@dhis2/ui'
+import { FC, useEffect, useState } from 'react'
 import { useDebouncedState } from '../hooks/use-debounce-state.ts'
 import classes from './searchable-single-select.module.css'
 
@@ -9,135 +9,73 @@ export interface Option {
     label: string
 }
 
-const Loader = forwardRef<HTMLDivElement, object>(function Loader(_, ref) {
-    return (
-        <div ref={ref} className={classes.loader}>
-            <CircularLoader />
-        </div>
-    )
-})
-
-function Error({ msg, onRetryClick }: { msg: string; onRetryClick: () => void }) {
-    return (
-        <div className={classes.error}>
-            <div className={classes.errorInnerWrapper}>
-                <span className={classes.loadingErrorLabel}>{msg}</span>
-                <button className={classes.errorRetryButton} type="button" onClick={onRetryClick}>
-                    {i18n.t('Retry')}
-                </button>
-            </div>
-        </div>
-    )
+interface OnChangeProps {
+    selected: string
 }
 
-type OnChange = ({ selected }: { selected: string }) => void
-type OnFilterChange = ({ value }: { value: string }) => void
-
-interface SearchableSingleSelectPropTypes {
-    onChange: OnChange
-    onFilterChange: OnFilterChange
-    onEndReached?: () => void
-    onRetryClick: () => void
-    dense?: boolean
+interface SearchableSingleSelectProps {
+    onChange: (props: OnChangeProps) => void
     options: Option[]
     placeholder: string
-    prefix?: string
-    showEndLoader: boolean
     loading: boolean
-    disabled?: boolean
     selected?: string
-    invalid?: boolean
-    error?: string
-    showAllOption?: boolean
-    onBlur?: () => void
-    onFocus?: () => void
+    setTagExists: (value: boolean) => void
+    tagExists: boolean
+    onTagCheck: (tag: string) => Promise<boolean>
+    refetch: () => void
 }
 
-export const SearchableSingleSelect = ({
-    invalid,
-    disabled,
-    error,
-    dense,
-    loading,
-    placeholder,
-    prefix,
-    onBlur,
-    onChange,
-    onEndReached,
-    onFilterChange,
-    onFocus,
-    onRetryClick,
-    options,
-    selected,
-    showAllOption,
-    showEndLoader,
-}: SearchableSingleSelectPropTypes) => {
-    const [loadingSpinnerRef, setLoadingSpinnerRef] = useState<HTMLElement>()
+export const SearchableSingleSelect: FC<SearchableSingleSelectProps> = ({ onChange, options, loading, selected, refetch, onTagCheck, placeholder, tagExists, setTagExists }) => {
+    const [inputValue, setInputValue] = useState<string>('')
 
-    const { liveValue: filter, setValue: setFilterValue } = useDebouncedState<string>({
-        initialValue: '',
-        onSetDebouncedValue: (value: string) => onFilterChange({ value }),
+    const { setValue: setDebouncedValue } = useDebouncedState({
+        initialValue: inputValue,
+        delay: 500,
+        onSetDebouncedValue: async (value: string) => {
+            if (value) {
+                const exists = await onTagCheck(value)
+                setTagExists(exists)
+            }
+        },
     })
 
-    useEffect(() => {
-        if (loadingSpinnerRef && !loading) {
-            const observer = new IntersectionObserver(
-                (entries) => {
-                    const [{ isIntersecting }] = entries
-                    if (isIntersecting) {
-                        onEndReached?.()
-                    }
-                },
-                { threshold: 0.8 }
-            )
-            observer.observe(loadingSpinnerRef)
-            return () => observer.disconnect()
-        }
-    }, [loadingSpinnerRef, loading, onEndReached])
+    const handleInputChange = ({ value }: { value: string }) => {
+        setInputValue(value)
+        setDebouncedValue(value)
+    }
 
-    const hasSelectedInOptionList = !!options.find(({ value }) => value === selected)
-    const withAllOptions = showAllOption ? [{ value: '', label: i18n.t('All') }, ...options] : options
+    useEffect(() => {
+        if (!inputValue) {
+            refetch()
+        }
+    }, [inputValue, refetch])
+
+    console.log(tagExists)
 
     return (
-        <SingleSelect
-            selected={hasSelectedInOptionList ? selected : ''}
-            disabled={disabled}
-            error={invalid}
-            onChange={onChange}
-            placeholder={placeholder}
-            prefix={prefix}
-            onBlur={onBlur}
-            onFocus={onFocus}
-            dense={dense}
-        >
-            <div className={classes.searchField}>
-                <div className={classes.searchInput}>
-                    <Input dense initialFocus value={filter} onChange={({ value }) => setFilterValue(value ?? '')} placeholder={i18n.t('Filter options')} />
+        <div>
+            <SingleSelect selected={selected} onChange={onChange} loading={loading}>
+                <div>
+                    <Input
+                        dense
+                        initialFocus
+                        value={inputValue}
+                        className={classes.searchField}
+                        onChange={handleInputChange}
+                        placeholder={i18n.t(placeholder || 'Type and press Enter')}
+                        onKeyDown={(_, event) => {
+                            if (event.key === 'Enter') {
+                                onTagCheck(inputValue)
+                            }
+                        }}
+                    />
                 </div>
-                <button className={classes.clearButton} disabled={!filter} onClick={() => setFilterValue('')}>
-                    clear
-                </button>
-            </div>
+                {!tagExists && inputValue && <div className={classes.error}>{i18n.t('Image tag does not exist')}</div>}
 
-            {withAllOptions.map(({ value, label }) => (
-                <SingleSelectOption key={value} value={value} label={label} />
-            ))}
-
-            {hasSelectedInOptionList && selected && <SingleSelectOption className={classes.invisibleOption} value={selected} label="" />}
-
-            {!error && !loading && showEndLoader && (
-                <Loader
-                    ref={(ref) => {
-                        if (!!ref && ref !== loadingSpinnerRef) {
-                            setLoadingSpinnerRef(ref)
-                        }
-                    }}
-                />
-            )}
-
-            {!error && loading && <Loader />}
-
-            {error && <Error msg={error} onRetryClick={onRetryClick} />}
-        </SingleSelect>
+                {options.map(({ value, label }) => (
+                    <SingleSelectOption key={value} value={value} label={label} />
+                ))}
+            </SingleSelect>
+        </div>
     )
 }
--- src/views/instances/new-dhis2/fields/image-tag-select.tsx
+++ src/views/instances/new-dhis2/fields/image-tag-select.tsx
@@ -1,6 +1,7 @@
-import { FC, useEffect, useState } from 'react'
+import { FC, useEffect, useState, useCallback } from 'react'
 import { useField, useForm } from 'react-final-form'
-import { SearchableSingleSelect } from '../../../../components/searchable-single-select.tsx'
+import classes from '../../../../components/searchable-single-select.module.css'
+import { SearchableSingleSelect, Option } from '../../../../components/searchable-single-select.tsx'
 import { useAuthAxios } from '../../../../hooks/index.ts'
 import { IMAGE_TAG } from '../constants.ts'
 import { IMAGE_REPOSITORY_FIELD_NAME } from './image-repository-select.tsx'
@@ -8,42 +9,28 @@ import { mapStringToValueLabel } from './map-string-to-value-label.tsx'
 
 const IMAGE_TAG_FIELD_NAME = `['dhis2-core'].${IMAGE_TAG}`
 
-export const ImageTagSelect: FC<{ displayName: string }> = ({ displayName }) => {
+interface ImageTagSelectProps {
+    displayName: string
+}
+
+export const ImageTagSelect: FC<ImageTagSelectProps> = ({ displayName }) => {
     const form = useForm()
-    const [options, setOptions] = useState<{ value: string; label: string }[]>([])
+    const [options, setOptions] = useState<Option[]>([])
+    const [tag, setTag] = useState<string>('')
+    const [tagExists, setTagExists] = useState<boolean>(false)
 
     const {
         meta: { initial: initialValue },
-    } = useField(IMAGE_TAG_FIELD_NAME, {
-        subscription: { initial: true },
-    })
+    } = useField<string>(IMAGE_TAG_FIELD_NAME, { subscription: { initial: true } })
+
     const {
         input: { value: repository },
-    } = useField(IMAGE_REPOSITORY_FIELD_NAME, {
-        subscription: { value: true },
-    })
+    } = useField<string>(IMAGE_REPOSITORY_FIELD_NAME, { subscription: { value: true } })
 
-    const [{ data, error, loading }, refetch] = useAuthAxios(
-        {
-            url: '/integrations',
-            method: 'POST',
-            data: {},
-        },
-        { manual: true, autoCatch: true }
-    )
+    const [{ data }, refetch] = useAuthAxios({ url: '/integrations', method: 'POST', data: {} }, { manual: true, autoCatch: true })
 
-    // Effect to handle setting options based on the data fetched
-    useEffect(() => {
-        if (data) {
-            if (loading) {
-                form.change(IMAGE_TAG_FIELD_NAME, undefined)
-                form.blur(IMAGE_TAG_FIELD_NAME)
-            }
-            setOptions(data.map(mapStringToValueLabel))
-        }
-    }, [data, loading, form])
+    const [{ loading: imageLoading }, checkImageExists] = useAuthAxios({ url: `/integrations/image-exists/${repository}/{tag}`, method: 'GET' }, { manual: true, autoCatch: false })
 
-    // Effect to trigger refetching options when the repository value changes
     useEffect(() => {
         if (repository) {
             refetch({
@@ -58,40 +45,80 @@ export const ImageTagSelect: FC<{ displayName: string }> = ({ displayName }) => 
         }
     }, [repository, refetch])
 
-    const handleFilterChange = ({ value }: { value: string }) => {
-        // Here, you would trigger the filtering of options by the search query
-        // You can debounce this input if necessary (it's already debounced by SearchableSingleSelect)
-        refetch({
-            data: {
-                key: IMAGE_TAG,
-                payload: {
-                    organization: 'dhis2',
-                    repository,
-                    filter: value,
-                },
-            },
-        })
-    }
+    useEffect(() => {
+        if (data) {
+            const mappedOptions = data.map(mapStringToValueLabel)
+            setOptions(mappedOptions)
 
-    // Handle option selection
+            const currentSelectedValue = form.getState().values['dhis2-core']?.IMAGE_TAG
+            if (currentSelectedValue && !mappedOptions.some((option) => option.value === currentSelectedValue)) {
+                form.change(IMAGE_TAG_FIELD_NAME, undefined)
+                form.blur(IMAGE_TAG_FIELD_NAME)
+            }
+        }
+    }, [data, form])
+
+    const handleTagCheck = useCallback(
+        async (tag: string): Promise<boolean> => {
+            const existsInOptions = options.some((option) => option.value === tag)
+            setTagExists(existsInOptions)
+
+            if (existsInOptions) {
+                setOptions((prevOptions) => {
+                    const filteredOptions = prevOptions.filter((option) => option.value !== tag)
+                    return [{ value: tag, label: tag }, ...filteredOptions]
+                })
+                return true
+            }
+
+            try {
+                const response = await checkImageExists({
+                    url: `/integrations/image-exists/${repository}/${tag}`,
+                })
+
+                if (response.status === 200) {
+                    // Add the new tag at the top of the options list
+                    setOptions((prevOptions) => {
+                        const filteredOptions = prevOptions.filter((option) => option.value !== tag)
+                        return [{ value: tag, label: tag }, ...filteredOptions]
+                    })
+
+                    setTag(tag)
+                    form.change(IMAGE_TAG_FIELD_NAME, tag)
+                    setTagExists(true)
+                    return true
+                }
+            } catch (error) {
+                if (error?.response?.status === 404) {
+                    setTag('')
+                    form.change(IMAGE_TAG_FIELD_NAME, undefined)
+                    setTagExists(false)
+                    return false
+                }
+            }
+            return false
+        },
+        [checkImageExists, form, repository, options]
+    )
+
     const handleChange = (selected: { selected: string }) => {
         form.change(IMAGE_TAG_FIELD_NAME, selected.selected)
     }
 
-    // Provide fallback if no options are available but there’s an initial value
-    const optionsWithFallback = options.length === 0 && initialValue ? [{ value: initialValue, label: initialValue }] : options
-
     return (
-        <SearchableSingleSelect
-            onChange={handleChange}
-            onFilterChange={handleFilterChange}
-            onRetryClick={() => refetch()}
-            options={optionsWithFallback}
-            loading={loading}
-            error={error ? error.message : undefined}
-            placeholder={displayName}
-            selected={form.getState().values['dhis2-core']?.IMAGE_TAG || undefined}
-            showEndLoader={false}
-        />
+        <div>
+            <label className={classes.label}>{displayName} *</label>
+            <SearchableSingleSelect
+                onChange={handleChange}
+                tagExists={tagExists}
+                setTagExists={setTagExists}
+                selected={form.getState().values['dhis2-core']?.IMAGE_TAG || tag || ''}
+                options={options.length === 0 && initialValue ? [{ value: initialValue, label: initialValue }] : options}
+                loading={imageLoading}
+                placeholder={displayName}
+                onTagCheck={handleTagCheck}
+                refetch={refetch}
+            />
+        </div>
     )
 }
