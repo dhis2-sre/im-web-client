5cff2bb9e4d175f8008020961a6fe8e13dce895d
tonsV2 <tonsV2@users.noreply.github.com>
2025-01-22 08:46:48 +0100

    chore: initial non-working custom ttl (#641)
    * chore: initial non-working custom ttl
    
    * feat: show custum calender
    
    * feat: create ttl and custom ttl component
    
    * fix: update date logic
    
    ---------
    
    Co-authored-by: Chisomchima <chimachisom360@gmail.com>

--- src/views/instances/new-dhis2/fields/ttl-select.tsx
+++ src/views/instances/new-dhis2/fields/ttl-select.tsx
@@ -1,9 +1,10 @@
-import { SingleSelectFieldFF, hasValue } from '@dhis2/ui'
-import { Field } from 'react-final-form'
+import { SingleSelectField, SingleSelectOption, CalendarInput } from '@dhis2/ui'
+import React, { useState } from 'react'
+import { useField } from 'react-final-form'
 import styles from './fields.module.css'
 
 const defaultTTL = 60 * 60 * 24
-const options: { label: string; value: string }[] = [
+const options = [
     { label: '1 hour', value: (60 * 60).toString() },
     { label: '6 hours', value: (60 * 60 * 6).toString() },
     { label: '12 hours', value: (60 * 60 * 12).toString() },
@@ -13,22 +14,54 @@ const options: { label: string; value: string }[] = [
     { label: '1 week', value: (60 * 60 * 24 * 7).toString() },
     { label: '2 weeks', value: (60 * 60 * 24 * 7 * 2).toString() },
     { label: '1 month', value: (60 * 60 * 24 * 7 * 4).toString() },
+    { label: 'Custom', value: '-1' },
 ]
-const parse = (str: string): number => parseInt(str)
-const format = (integer: number): string => integer.toString()
 
-export const TtlSelect = () => (
-    <Field
-        className={styles.field}
-        parse={parse}
-        format={format}
-        required
-        name="ttl"
-        label="Lifetime"
-        component={SingleSelectFieldFF}
-        options={options}
-        initialValue={defaultTTL}
-        validate={hasValue}
-        helpText="How long this instance will run for until automatic shutdown."
-    />
-)
+export const TtlSelect = () => {
+    const { input: ttlInput } = useField('ttl', { subscription: { value: true } })
+    const [selectedValue, setSelectedValue] = useState(ttlInput.value.toString())
+    const [showCalendar, setShowCalendar] = useState(false)
+    const [calendarDate, setCalendarDate] = useState('')
+
+    const handleSelectChange = (value: string) => {
+        setSelectedValue(value)
+        if (value === '-1') {
+            setShowCalendar(true)
+        } else {
+            ttlInput.onChange(parseInt(value, 10))
+            setShowCalendar(false)
+        }
+    }
+
+    const handleCustomDateChange = (date) => {
+        const currentDate = new Date()
+        const targetDate = new Date(date.calendarDateString + 'T00:00:00Z')
+        const differenceInMs = targetDate.getTime() - currentDate.getTime()
+
+        const differenceInSeconds = Math.floor(differenceInMs / 1000)
+        ttlInput.onChange(differenceInSeconds)
+        setSelectedValue('-1')
+        setCalendarDate(targetDate.toISOString().split('T')[0])
+    }
+
+    return (
+        <div>
+            <SingleSelectField
+                className={styles.field}
+                label="Lifetime"
+                selected={selectedValue}
+                onChange={({ selected }: { selected: string }) => handleSelectChange(selected)}
+                helpText="How long this instance will run for until automatic shutdown."
+            >
+                {options.map((option) => (
+                    <SingleSelectOption key={option.value} label={option.label} value={option.value} />
+                ))}
+            </SingleSelectField>
+            {showCalendar && (
+                <div className={styles.calenderContainer}>
+                    <CalendarInput onDateSelect={handleCustomDateChange} calendar="iso8601" date={calendarDate} />
+                </div>
+            )}
+        </div>
+    )
+}
