5d763d66a57bd681ee85f45dfd05423e8af01221
tonsV2 <tonsV2@users.noreply.github.com>
2025-01-29 05:57:28 +0100

    chore: e2e test copy and rename (#661)
    * chore: click menu item rather than button
    
    * chore: fix name
    
    * fix: dialog text
    
    * chore: include extension
    
    * test: copy/rename

--- e2e/databases.spec.ts
+++ e2e/databases.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test'
-import { login, logout } from './utils/index.ts'
+import { deleteTestDatabase, login, logout } from './utils/index.ts'
 
 test.describe('databases', () => {
     test.beforeEach(async ({ page }) => {
@@ -11,5 +11,37 @@ test.describe('databases', () => {
 
     test('has a database section', async ({ page }) => {
         await expect(page.getByRole('link', { name: 'Databases' })).toBeVisible()
+    })
+
+    test('copy/rename database', async ({ page }) => {
+        await page.getByRole('link', { name: 'Databases' }).click()
+
+        const firstRow = page.locator('table[data-test="dhis2-uicore-datatable"] tbody tr:first-child')
+        await firstRow.getByTestId('dhis2-uicore-button').click()
+        await page.getByRole('menuitem', { name: 'Copy' }).click()
+
+        // Copy
+        const inputName = page.locator('div[data-test="dhis2-uicore-input"] input')
+        const name = await inputName.inputValue()
+        const newName = `copy-${name}`
+        await inputName.fill(newName)
+        await page.selectOption('#group-select', 'whoami')
+        await page.locator('button').getByText('Copy').click()
+
+        // Rename
+        const copiedDatabase = page.getByRole('row', { name: newName })
+        await copiedDatabase.getByTestId('dhis2-uicore-button').click()
+        await page.getByRole('menuitem', { name: 'Rename' }).click()
+        const inputRename = page.locator('div[data-test="dhis2-uicore-input"] input')
+        const rename = `rename-${newName}`
+        await inputRename.fill(rename)
+        await page.locator('button').getByText('Rename').click()
+
+        // Delete
+        await deleteTestDatabase(page, rename)
+
+        // Confirm deletion
+        const cellLocator = page.getByRole('cell', { name: rename })
+        await expect(cellLocator).toHaveCount(0)
     })
 })
