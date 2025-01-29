e16de5f523da749190ec0fe06a62c6c01896b8fe
Andreas Jensen <andreas@dhis2.org>
2025-01-29 09:21:50 +0700

    chore: fix name

--- e2e/utils/delete-database.ts
+++ e2e/utils/delete-database.ts
@@ -1,10 +1,9 @@
 import { expect } from '@playwright/test'
 
-const defaultDbName = 'empty-db'
-const dbNameExt = 'sql.gz'
+const defaultDbName = 'empty-db.sql.gz'
 
 export const deleteTestDatabase = async (page, dbName = defaultDbName) => {
-    const dbFileName = `${dbName}.${dbNameExt}`
+    const dbFileName = `${dbName}`
 
     await page.getByRole('link', { name: 'Databases' }).click()
     await expect(page.getByRole('button', { name: 'Upload database' })).toBeVisible()
