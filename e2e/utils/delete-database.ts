10c2cb84edc93c4b17ac0119d3ec3e14a9c24962
Chisomchima <chimachisom360@gmail.com>
2025-02-06 12:04:30 +0100

    fix: adding tests

--- e2e/utils/delete-database.ts
+++ e2e/utils/delete-database.ts
@@ -1,13 +1,20 @@
 import { expect } from '@playwright/test'
 
-export const deleteTestDatabase = async (page, dbName: string) => {
-    await page.route(`/databases/${dbName}`, async (route) => {
-        await route.fulfill({ status: 200, body: JSON.stringify({ success: true }) })
-    })
+const defaultDbName = 'empty-db.sql.gz'
 
+export const deleteTestDatabase = async (page, dbName = defaultDbName) => {
+    const dbFileName = `${dbName}`
+
+    await page.getByRole('link', { name: 'Databases' }).click()
+    await expect(page.getByRole('button', { name: 'Upload database' })).toBeVisible()
+
+    await expect(page.getByRole('cell', { name: dbFileName })).toBeVisible()
+    const testDatabaseRow = page.getByRole('row', { name: dbFileName })
+    await testDatabaseRow.getByTestId('dhis2-uicore-button').first().click()
     await page.getByRole('menuitem', { name: 'Delete' }).click()
-    await expect(page.getByRole('dialog')).toBeVisible()
+
+    await expect(page.getByTestId('dhis2-uicore-modalcontent')).toContainText(dbFileName)
     await page.getByRole('button', { name: 'Confirm' }).click()
 
-    await expect(page.getByText(`Successfully deleted [^/]+/${dbName}`)).toBeVisible()
+    await expect(page.getByTestId('dhis2-uicore-alertbar').getByText(new RegExp(`Successfully deleted [^/]+/${dbFileName}`))).toBeVisible()
 }
