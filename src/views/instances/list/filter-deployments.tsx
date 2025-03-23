d0ce7d55faad62cf8f25fa6140aafcfa10082f26
Chisom Chima <87203527+Chisomchima@users.noreply.github.com>
2025-03-23 19:34:36 +0100

    improve local storage

--- src/views/instances/list/filter-deployments.tsx
+++ src/views/instances/list/filter-deployments.tsx
@@ -1,28 +1,34 @@
-import { useState, useMemo, useContext, useEffect } from 'react'
+import { useState, useMemo, useContext, useCallback } from 'react'
 import { AuthContext } from '../../../contexts/auth-context.ts'
 import { useAuthAxios } from '../../../hooks/index.ts'
 import { GroupWithDeployments } from '../../../types/index.ts'
 
 const useDeployments = () => {
+    const LOCAL_STORAGE_KEY = 'showOnlyMyInstances'
     const [{ data, error, loading }, refetch] = useAuthAxios<GroupWithDeployments[]>('/deployments', {
         useCache: false,
     })
 
-    const [showOnlyMyInstances, setShowOnlyMyInstances] = useState(false)
+    const [showOnlyMyInstances, _setShowOnlyMyInstances] = useState<boolean>(() => {
+        try {
+            return !!JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+        } catch {
+            return false
+        }
+    })
+
+    const setShowOnlyMyInstances: typeof _setShowOnlyMyInstances = useCallback(
+        (showOnlyMyInstances) => {
+            return _setShowOnlyMyInstances((prev) => {
+                const resolvedValue = typeof showOnlyMyInstances === 'function' ? showOnlyMyInstances(prev) : showOnlyMyInstances
+                localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(resolvedValue))
+                return resolvedValue
+            })
+        },
+        [_setShowOnlyMyInstances]
+    )
+
     const { currentUser } = useContext(AuthContext)
-
-    useEffect(() => {
-        const savedShowOnlyMyInstances = localStorage.getItem('showOnlyMyInstances')
-        if (savedShowOnlyMyInstances !== null) {
-            setShowOnlyMyInstances(JSON.parse(savedShowOnlyMyInstances))
-        }
-    }, [])
-
-    useEffect(() => {
-        if (showOnlyMyInstances) {
-            localStorage.setItem('showOnlyMyInstances', JSON.stringify(showOnlyMyInstances))
-        }
-    }, [showOnlyMyInstances])
 
     const filteredData = useMemo(() => {
         let filteredGroups = data?.map((group) => ({
