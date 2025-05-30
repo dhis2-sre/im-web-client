81f14e0a0f5817683a9569f1c8f4ce1e2936848d
Andreas Jensen <andreas@dhis2.org>
2025-05-31 02:45:49 +0700

    feat: show group resources

--- src/views/instances/new-dhis2/fields/group-select.tsx
+++ src/views/instances/new-dhis2/fields/group-select.tsx
@@ -1,12 +1,41 @@
-import { SingleSelectFieldFF, hasValue } from '@dhis2/ui'
-import { useEffect, useMemo } from 'react'
-import { Field, useForm } from 'react-final-form'
+import { Center, CircularLoader, hasValue, SingleSelectFieldFF } from '@dhis2/ui'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import { Field, useForm, useFormState } from 'react-final-form'
 import { useAuthAxios } from '../../../../hooks/index.ts'
-import { Group } from '../../../../types/index.ts'
+import { ClusterResources, Group } from '../../../../types/index.ts'
 import styles from './fields.module.css'
 
 export const GroupSelect = () => {
     const form = useForm()
+    const { values } = useFormState()
+    const groupNameValue = values.groupName
+    const [resources, setResources] = useState<ClusterResources>({})
+    const [resourcesString, setResourcesString] = useState<string>('')
+    const [{ loading: clusterResourcesLoading, error: clusterResourcesError }, fetchClusterResources] = useAuthAxios<ClusterResources>(
+        {
+            method: 'GET',
+        },
+        { manual: true, autoCatch: true }
+    )
+
+    const fetchClusterResourcesCallback = useCallback(async () => {
+        const { data } = await fetchClusterResources(`/groups/${groupNameValue}/resources`)
+        setResources(data)
+    }, [fetchClusterResources, groupNameValue])
+
+    useEffect(() => {
+        try {
+            void fetchClusterResourcesCallback()
+        } catch (e) {
+            console.error(e)
+        }
+    }, [fetchClusterResourcesCallback, groupNameValue])
+
+    useEffect(() => {
+        const resourcesStr = `CPU ${resources.CPU}, Memory ${resources.Memory}${resources.Autoscaled ? ', autoscaled' : ''}`
+        setResourcesString(resourcesStr)
+    }, [resources])
+
     const [{ data: groups, loading, error }] = useAuthAxios<Group[]>({
         method: 'GET',
         url: '/groups',
@@ -14,6 +43,21 @@ export const GroupSelect = () => {
             deployable: true,
         },
     })
+
+    const helpText = useMemo(() => {
+        if (clusterResourcesError) {
+            return `Error fetching cluster resources: ${clusterResourcesError.message}`
+        }
+        if (clusterResourcesLoading) {
+            return (
+                <Center>
+                    <CircularLoader />
+                </Center>
+            )
+        }
+        return resourcesString
+    }, [clusterResourcesError, clusterResourcesLoading, resourcesString])
+
     const options = useMemo(() => (groups ?? []).map(({ name }) => ({ label: name, value: name })), [groups])
 
     useEffect(() => {
@@ -24,16 +68,19 @@ export const GroupSelect = () => {
     }, [groups, form])
 
     return (
-        <Field
-            filterable={options.length > 7}
-            className={styles.field}
-            required={loading}
-            error={error}
-            name="groupName"
-            label="Group"
-            component={SingleSelectFieldFF}
-            options={options}
-            validate={hasValue}
-        />
+        <>
+            <Field
+                filterable={options.length > 7}
+                className={styles.field}
+                required={loading}
+                error={error}
+                name="groupName"
+                label="Group"
+                component={SingleSelectFieldFF}
+                options={options}
+                helpText={helpText}
+                validate={hasValue}
+            />
+        </>
     )
 }
