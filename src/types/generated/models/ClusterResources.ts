5f1ddf780b08e62303428d1390b938915bbddf1f
Andreas Jensen <andreas@dhis2.org>
2025-05-31 13:59:24 +0700

    chore: generate types

---
+++ src/types/generated/models/ClusterResources.ts
@@ -0,0 +1,10 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+
+export type ClusterResources = {
+    Autoscaled?: boolean
+    CPU?: string
+    Memory?: string
+    Nodes?: number
+}
