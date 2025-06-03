01012b36711b8bc26674cf76ee41f7425cd205e6
tonsV2 <tonsV2@users.noreply.github.com>
2025-06-04 02:15:13 +0800

    feat: show group resources (#691)
    * feat: show group resources
    
    * refactor: introduce GroupSelectHelpText component
    
    * chore: remove redundant parent element
    
    * chore: show small loader

---
+++ src/views/instances/new-dhis2/fields/group-select-help-text.tsx
@@ -0,0 +1,35 @@
+import { Center, CircularLoader } from '@dhis2/ui'
+import { useCallback, useEffect, useState } from 'react'
+import { useAuthAxios } from '../../../../hooks/index.ts'
+import { ClusterResources } from '../../../../types/index.ts'
+
+type GroupSelectHelpTextProps = {
+    groupName: string
+}
+
+export const GroupSelectHelpText = ({ groupName }: GroupSelectHelpTextProps) => {
+    const [resources, setResources] = useState<ClusterResources>({})
+    const [{ loading, error }, fetchClusterResources] = useAuthAxios<ClusterResources>({ method: 'GET' }, { manual: true, autoCatch: true })
+    const loadResources = useCallback(async () => {
+        const { data } = await fetchClusterResources(`/groups/${groupName}/resources`)
+        setResources(data)
+    }, [fetchClusterResources, groupName])
+
+    useEffect(() => {
+        void loadResources()
+    }, [loadResources, groupName])
+
+    if (error) {
+        return `Error fetching cluster resources: ${error.message}`
+    }
+
+    if (loading) {
+        return (
+            <Center>
+                <CircularLoader small />
+            </Center>
+        )
+    }
+
+    return `CPU ${resources.CPU}, Memory ${resources.Memory}${resources.Autoscaled ? ', autoscaled' : ''}`
+}
--- src/views/instances/new-dhis2/fields/group-select.tsx
+++ src/views/instances/new-dhis2/fields/group-select.tsx
@@ -1,12 +1,15 @@
-import { SingleSelectFieldFF, hasValue } from '@dhis2/ui'
-import { useEffect, useMemo } from 'react'
+import { hasValue, SingleSelectFieldFF } from '@dhis2/ui'
+import { useEffect, useMemo, useState } from 'react'
 import { Field, useForm } from 'react-final-form'
 import { useAuthAxios } from '../../../../hooks/index.ts'
 import { Group } from '../../../../types/index.ts'
 import styles from './fields.module.css'
+import { GroupSelectHelpText } from './group-select-help-text.tsx'
 
 export const GroupSelect = () => {
     const form = useForm()
+    const [currentGroup, setCurrentGroup] = useState()
+
     const [{ data: groups, loading, error }] = useAuthAxios<Group[]>({
         method: 'GET',
         url: '/groups',
@@ -14,7 +17,17 @@ export const GroupSelect = () => {
             deployable: true,
         },
     })
+
     const options = useMemo(() => (groups ?? []).map(({ name }) => ({ label: name, value: name })), [groups])
+
+    useEffect(() => {
+        return form.subscribe(
+            ({ values }) => {
+                setCurrentGroup(values.groupName)
+            },
+            { values: true }
+        )
+    }, [form])
 
     useEffect(() => {
         const { value } = form.getFieldState('groupName')
@@ -33,6 +46,7 @@ export const GroupSelect = () => {
             label="Group"
             component={SingleSelectFieldFF}
             options={options}
+            helpText={<GroupSelectHelpText groupName={currentGroup} />}
             validate={hasValue}
         />
     )
