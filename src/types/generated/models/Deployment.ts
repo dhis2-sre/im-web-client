e807747133ea002bf36cba4993ffd845fcfbaf0b
Chisomchima <chimachisom360@gmail.com>
2025-09-19 19:46:15 +0100

    chore: update generate types

--- src/types/generated/models/Deployment.ts
+++ src/types/generated/models/Deployment.ts
@@ -1,7 +1,7 @@
 /* generated using openapi-typescript-codegen -- do not edit */
 /* istanbul ignore file */
 /* tslint:disable */
-/* eslint-disable */
+ 
 import type { DeploymentInstance } from './DeploymentInstance';
 import type { Group } from './Group';
 import type { User } from './User';
