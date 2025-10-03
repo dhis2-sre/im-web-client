a7b08a7d82a3cf061f906e7691f65f93c0d24561
Andreas Jensen <andreas@dhis2.org>
2025-10-03 11:44:01 +0700

    chore: generate types

--- src/types/generated/models/Database.ts
+++ src/types/generated/models/Database.ts
@@ -7,7 +7,6 @@ import type { Lock } from './Lock'
 import type { User } from './User'
 export type Database = {
     createdAt?: string
-    description?: string
     externalDownloads?: Array<ExternalDownload>
     filestore?: Database
     filestoreId?: number
--- src/types/generated/models/UpdateDatabaseRequest.ts
+++ src/types/generated/models/UpdateDatabaseRequest.ts
@@ -3,6 +3,5 @@
 /* tslint:disable */
 /* eslint-disable */
 export type UpdateDatabaseRequest = {
-    description?: string
     name?: string
 }
