1795a3675848cc10c29a85e221041b3823d7cf2c
Andreas Jensen <andreas@dhis2.org>
2025-11-13 02:00:54 +0700

    chore: construct open url with the new group id

--- src/views/instances/list/open-button.tsx
+++ src/views/instances/list/open-button.tsx
@@ -1,19 +1,24 @@
 import { Button, IconLaunch16 } from '@dhis2/ui'
-import { useCallback } from 'react'
 import type { FC } from 'react'
+import { useCallback } from 'react'
+import { Deployment } from '../../../types'
 
 type OpenButtonProps = {
-    hostname: string
-    name: string
+    deployment: Deployment
 }
 
-export const OpenButton: FC<OpenButtonProps> = ({ hostname, name }) => {
+export const OpenButton: FC<OpenButtonProps> = ({ deployment }) => {
+    const group = deployment.group
+    const hostname = group.hostname
+    const groupId = group.id
+    const name = deployment.name
+
     const onClick = useCallback(
         (_, event) => {
             event.stopPropagation()
-            window.open(`https://${hostname}/${name}`)
+            window.open(`https://${hostname}/${name}-${groupId}`)
         },
-        [hostname, name]
+        [hostname, name, groupId]
     )
 
     return (
