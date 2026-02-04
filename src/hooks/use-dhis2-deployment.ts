74e0b970a4775f1c71dd960c0b85b802c12bc473
Andreas Jensen <andreas@dhis2.org>
2026-02-04 17:40:23 +0700

    chore: consume DATABASE_ID automatically

--- src/hooks/use-dhis2-deployment.ts
+++ src/hooks/use-dhis2-deployment.ts
@@ -78,8 +78,6 @@ export const useDhis2DeploymentCreation = () => {
                 const deploymentId = await saveDeployment(values)
                 await addStackToDeployment(deploymentId, STACK_NAMES.DB, values)
                 if (values[STACK_NAMES.CORE].STORAGE_TYPE === 'minio') {
-                    console.log(values[STACK_NAMES.MINIO])
-                    values[STACK_NAMES.MINIO].DATABASE_ID = values[STACK_NAMES.DB].DATABASE_ID
                     await addStackToDeployment(deploymentId, STACK_NAMES.MINIO, values)
                 }
                 await addStackToDeployment(deploymentId, STACK_NAMES.CORE, values)
