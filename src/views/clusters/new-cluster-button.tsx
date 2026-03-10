8f7cc6cef0c2cd641dd8d57620594c33d409fb75
tonsV2 <tonsV2@users.noreply.github.com>
2026-03-10 18:02:31 +0700

    feat: create clusters (#738)
    * feat: create clusters

--- src/views/clusters/new-cluster-button.tsx
+++ src/views/clusters/new-cluster-button.tsx
@@ -1,16 +1,28 @@
 import { Button, IconAdd24 } from '@dhis2/ui'
 import type { FC } from 'react'
+import { useCallback, useState } from 'react'
+import { NewClusterModal } from './new-cluster-modal.tsx'
 
 type NewClusterButtonProps = {
     onComplete: () => void
 }
 
-// TODO: See new NewGroupButton for inspiration
-
 export const NewClusterButton: FC<NewClusterButtonProps> = ({ onComplete }) => {
+    const [showModal, setShowModal] = useState<boolean>(false)
+    const closeModal = useCallback(() => {
+        setShowModal(false)
+    }, [])
+    const closeModalAndFetchList = useCallback(() => {
+        setShowModal(false)
+        onComplete()
+    }, [onComplete])
+
     return (
-        <Button icon={<IconAdd24 />} onClick={() => onComplete()}>
-            New cluster
-        </Button>
+        <>
+            <Button icon={<IconAdd24 />} onClick={() => setShowModal(true)}>
+                New cluster
+            </Button>
+            {showModal && <NewClusterModal onCancel={closeModal} onComplete={closeModalAndFetchList} />}
+        </>
     )
 }
