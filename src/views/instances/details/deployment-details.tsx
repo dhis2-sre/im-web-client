6a748c91604310328eca78d8630f13f43498dcfc
tonsV2 <tonsV2@users.noreply.github.com>
2026-08-03 11:15:45 +0200

    fix: bring back the deployment stacks widget on the details page (#887)

--- src/views/instances/details/deployment-details.tsx
+++ src/views/instances/details/deployment-details.tsx
@@ -5,6 +5,7 @@ import { Heading } from '../../../components/index.ts'
 import { useDeploymentDetails } from '../../../hooks/index.ts'
 import { DeploymentComponents } from './deployment-components.tsx'
 import styles from './deployment-details.module.css'
+import { DeploymentInstancesList } from './deployment-instances-list.tsx'
 import { DeploymentSummary } from './deployment-summary.tsx'
 
 export const DeploymentDetails: FC = () => {
@@ -41,6 +42,7 @@ export const DeploymentDetails: FC = () => {
                     {!deployment?.instances?.length && (
                         <NoticeBox title="No stacks connected to this instance">Currently you can only add components to an instance when creating one.</NoticeBox>
                     )}
+                    {deployment?.instances?.length > 0 && <DeploymentInstancesList deployment={deployment} loading={loading} />}
                     {deployment?.instances?.length > 0 && <DeploymentComponents deployment={deployment} />}
                 </>
             )}
---
+++ src/views/instances/details/status-label.tsx
@@ -0,0 +1,34 @@
+import { CircularLoader, Tag } from '@dhis2/ui'
+import type { FC } from 'react'
+import { useAuthAxios } from '../../../hooks/index.ts'
+import { getTagProps } from '../../../utils/tag.tsx'
+import styles from './status-label.module.css'
+
+type Status = 'NotDeployed' | 'Pending' | 'Booting' | 'Booting (%d)' | 'Running' | 'Error'
+
+const Content: FC<{ status: string; loading: boolean }> = ({ status, loading }) => {
+    if (loading) {
+        return <CircularLoader extrasmall />
+    } else if (status) {
+        return status
+    } else {
+        return 'Unknown'
+    }
+}
+
+export const StatusLabel: FC<{
+    instanceId: number
+}> = ({ instanceId }) => {
+    const [{ data: status, loading }] = useAuthAxios<Status>({
+        method: 'GET',
+        url: `/instances/${instanceId}/status`,
+    })
+
+    return (
+        <span className={styles.wrapper}>
+            <Tag {...getTagProps(status)}>
+                <Content status={status} loading={loading} />
+            </Tag>
+        </span>
+    )
+}
